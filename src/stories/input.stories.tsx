02bb88cd5b58f60f62c62766aa68d3d770cbe6f7
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-07-17 17:56:52 -0600

    13 - controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, createRef, SetStateAction, useRef, useState} from "react";
+import React, {ChangeEvent, createRef, SetStateAction, useEffect, useRef, useState} from "react";
 
 export default {
     title: 'input'
@@ -22,13 +22,50 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 
     const onClickBtnHandler = () => {
         /*const el = inputRef.current as HTMLInputElement*/
-        /*setValue(el.value)*/
+        /*setValue(el.value)*/ //or
         setValue(inputRef.current!.value)
     }
 
     return <><input ref={inputRef}/>
-        <button onClick={onClickBtnHandler}>save
-        </button>
-        -
-        actual value : {value}</>
+        <button onClick={onClickBtnHandler}>save</button>
+        - actual value : {value}</>
 }
+
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState<string>('')
+
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <input value={parentValue} onChange={onChangeInputHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState<boolean>(true)
+
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+
+    return <input type={"checkbox"} onChange={onChangeInputHandler} checked={parentValue}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    
+    const onChangeInputHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return (
+        <select value={parentValue} onChange={onChangeInputHandler}>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Kiev</option>
+            <option value={'3'}>Ottawa</option>
+        </select>
+    )
+}
+
+
